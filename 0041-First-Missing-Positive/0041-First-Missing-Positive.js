948002dfb914c81b2a80f11de6f1d90185a20098
boomboxik <9067506@gmail.com>
2024-06-01 18:40:36 +0300

    resolve LeetCode task #41

---
+++ 0041-First-Missing-Positive/0041-First-Missing-Positive.js
@@ -0,0 +1,18 @@
+function firstMissingPositive(nums) {
+    let n = nums.length;
+
+    for (let i = 0; i < n; i++) {
+        while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] !== nums[i]) {
+            let correctIndex = nums[i] - 1;
+            [nums[i], nums[correctIndex]] = [nums[correctIndex], nums[i]]
+        }
+    }
+
+    for (let i = 0; i < n; i++) {
+        if (nums[i] !== i + 1) {
+            return i + 1;
+        }
+    }
+
+    return n + 1;
+}
