10616da966dd7f8264e95fe70fc9c37eebde31a5
boomboxik <9067506@gmail.com>
2024-06-02 17:09:36 +0300

    resolve LeetCode task #42

---
+++ 0042-Trapping-Rain-Water/0042-Trapping-Rain-Water.js
@@ -0,0 +1,27 @@
+function trap(height) {
+    if (height.length === 0) return 0;
+
+    let left = 0, right = height.length - 1;
+    let leftMax = 0, rightMax = 0;
+    let totalWater = 0;
+
+    while (left < right) {
+        if (height[left] < height[right]) {
+            if (height[left] >= leftMax) {
+                leftMax = height[left];
+            } else {
+                totalWater += leftMax - height[left];
+            }
+            left++;
+        } else {
+            if (height[right] >= rightMax) {
+                rightMax = height[right];
+            } else {
+                totalWater += rightMax - height[right];
+            }
+            right--;
+        }
+    }
+
+    return totalWater;
+}
