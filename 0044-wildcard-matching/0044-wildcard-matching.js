1e02f6c1bb6a49899d65c28584ed9c7b8cd3ed1f
boomboxik <145867496+boomboxik@users.noreply.github.com>
2024-06-08 16:43:05 +0300

    Time: 131 ms (66.93%), Space: 66.7 MB (43.2%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.js
@@ -0,0 +1,26 @@
+function isMatch(s, p) {
+    const m = s.length;
+    const n = p.length;
+
+    const dp = Array.from({ length: m + 1 }, () => Array(n + 1).fill(false));
+
+    dp[0][0] = true;
+
+    for (let j = 1; j <= n; j++) {
+        if (p[j - 1] === '*') {
+            dp[0][j] = dp[0][j - 1];
+        }
+    }
+
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+            if (p[j - 1] === '?' || p[j - 1] === s[i - 1]) {
+                dp[i][j] = dp[i - 1][j - 1];
+            } else if (p[j - 1] === '*') {
+                dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+            }
+        }
+    }
+
+    return dp[m][n];
+}
