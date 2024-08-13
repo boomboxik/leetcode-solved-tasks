126923b24f0ebf7fa8a776f2de54361772686693
boomboxik <145867496+boomboxik@users.noreply.github.com>
2024-08-13 16:14:15 +0300

    Time: 52 ms (69.81%), Space: 48.9 MB (68.77%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.js
@@ -0,0 +1,12 @@
+function uniquePaths(m, n) {
+
+    let dp = Array(n).fill(1);
+
+    for (let i = 1; i < m; i++) {
+        for (let j = 1; j < n; j++) {
+            dp[j] = dp[j] + dp[j - 1];
+        }
+    }
+
+    return dp[n - 1];
+}
